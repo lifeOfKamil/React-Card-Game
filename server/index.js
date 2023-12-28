bb0cc0db21161258e377e947905cbf3a0190c422
Kamil Lepkowski <lepkowskikamil@yahoo.com>
2023-12-28 11:15:50 -0600

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const http = require("http").Server(app);
+const cors = require("cors");
+
+const socketIO = require("socket.io")(http, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
+
+socketIO.on("connection", (socket) => {
+	console.log(`⚡: ${socket.id} user just connected!`);
+	socket.on("disconnect", () => {
+		console.log("A user disconnected");
+	});
+});
+
+app.get("/api", (req, res) => {
+	res.json({
+		message: "Hello from server!",
+	});
+});
+
+app.listen(PORT, () => {
+	console.log(`Server listening on ${PORT}`);
+});
